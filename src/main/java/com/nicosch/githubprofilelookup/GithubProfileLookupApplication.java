f6d53426eb20b12d382029babf1b4e3305857838
Nico Schulterer <91744414+Nico-Schulterer@users.noreply.github.com>
2026-01-08 22:38:29 +0100

    Fixed wrong git root

---
+++ src/main/java/com/nicosch/githubprofilelookup/GithubProfileLookupApplication.java
@@ -0,0 +1,13 @@
+package com.nicosch.githubprofilelookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubProfileLookupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubProfileLookupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicosch/githubprofilelookup/GithubProfileLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicosch.githubprofilelookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProfileLookupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
